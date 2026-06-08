9168ddf9e982491cfba2b82f5de50c67bcc7db5c
hamzasou <50087699+hamzasou@users.noreply.github.com>
2026-06-08 15:41:11 +0100

    Add files via upload

--- run_mnist_12flows.py
+++ run_mnist_12flows.py
@@ -459,8 +459,8 @@ def main():
     # 500 est beaucoup moins lourd que 1000 et reste correct
     num_projections = 2000
 
-    lamb = 1e-3
-    gamma = 1e-2
+    lamb = 1.7e-4
+    gamma = 5e-1
 
     noise_std = 0.7
 
