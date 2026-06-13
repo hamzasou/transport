39bf2802d35bba0b7caf03dabb260b48c42ff843
hamzasou <50087699+hamzasou@users.noreply.github.com>
2026-06-13 17:37:24 +0100

    Add files via upload

--- run_mnist_12flows.py
+++ run_mnist_12flows.py
@@ -350,10 +350,10 @@ def main():
     epochs = 450
 
     dim = 28 * 28
-    nb_flows = 20
+    nb_flows = 
     hidden_dim = 512
     lr = 3e-5
-    num_projections = 2500
+    num_projections = 2000
 
     lamb = 5.2e-5
     gamma = 5.8e-2
